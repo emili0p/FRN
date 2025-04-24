5d0f1eb73c3dc075e563ac53ae1deb6424dfc170
Emilio Izquierdo Montero <emiliioizq23@gmail.com>
2025-04-23 20:46:14 -0600

    Creado un entorno basico falta añadirlo a evaluar

---
+++ src/entorno/entorno.c
@@ -0,0 +1,38 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "src\entorno\entorno.h"
+
+Entorno *crearentorno(){
+    return NULL;
+} // Crea una lista vacia como entorno, ahi en esa lista guardaremos cosas
+
+void definir_var(Entorno **env, const char *nombre, Objeto *valor){
+    Entorno *nodo = malloc(sizeof(Entorno));
+    nodo->nombre = strdup(nombre);
+    nodo->valor = valor;
+    nodo->siguiente = *env;
+    *env = nodo;
+}
+
+Objeto *buscar_variable(Entorno *env, const char nombre){
+    while (env != NULL){ //entra solo si en entorno hay algo si no sale 
+        if (strcmp(env->nombre, nombre == 0)){
+            return env->valor;
+        }
+        env = env->siguiente;
+    }
+    printf("error la variable no esta definida %s \n" );
+    exit(1);
+}
+
+void liberar_entorno(Entorno *env){
+    while (env) //mientras exista env se borrara el mismo
+    {
+        Entorno *tmp = env;
+        env= env->siguiente;
+        free(tmp->nombre);
+        free(tmp);
+    }
+    
+}
---
+++ src/entorno/entorno.h
@@ -0,0 +1,21 @@
+// entorno.h
+
+/* mapeo de las variables del entorno */
+#ifndef ENTORNO_H
+#define ENTORNO_H
+
+#include "src\evaluador\evaluador.h"
+
+typedef struct Entorno {
+    char *nombre;
+    Objeto *valor;
+    struct Entorno *siguiente;
+} Entorno;
+
+// funciones
+Entorno *crear_entorno();
+void definir_variable(Entorno **env, const char *nombre, Objeto *valor);
+Objeto *buscar_variable(Entorno *env, const char *nombre);
+void liberar_entorno(Entorno *env);
+
+#endif
