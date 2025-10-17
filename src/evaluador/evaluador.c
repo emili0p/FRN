06df64de349797762f735fcf944671d3ff0b406e
emili0p <emiliioizq23@gmail.com>
2025-10-17 11:32:00 -0600

    Arreglado el evaluador

--- src/evaluador/evaluador.c
+++ src/evaluador/evaluador.c
@@ -1,14 +1,12 @@
-/*
-definimos los metodos que el evaluador con los tokens pasados en el parser y su
-tipo de objeto
-*/
-#include "evaluador.h"
+#include "../evaluador/evaluador.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 Objeto *nuevo_num(double valor) {
   Objeto *o = malloc(sizeof(Objeto));
+  if (!o)
+    return NULL;
   o->tipo = TIPO_NUMERO;
   o->numero = valor;
   return o;
@@ -16,6 +14,8 @@ Objeto *nuevo_num(double valor) {
 
 Objeto *nuevo_simbolo(const char *nombre) {
   Objeto *o = malloc(sizeof(Objeto));
+  if (!o)
+    return NULL;
   o->tipo = TIPO_SIMBOLO;
   o->simbolo = strdup(nombre);
   return o;
@@ -23,15 +23,19 @@ Objeto *nuevo_simbolo(const char *nombre) {
 
 Objeto *nueva_funcion(FuncionIntegrada fn) {
   Objeto *o = malloc(sizeof(Objeto));
+  if (!o)
+    return NULL;
   o->tipo = TIPO_FUNCION_INTEGRADA;
   o->funcion = fn;
   return o;
 }
 
-Objeto *nueva_lista(Objeto **elemento, int cantidad) {
+Objeto *nueva_lista(Objeto **elementos, int cantidad) {
   Objeto *o = malloc(sizeof(Objeto));
+  if (!o)
+    return NULL;
   o->tipo = TIPO_LISTA;
-  o->lista.elementos = elemento;
+  o->lista.elementos = elementos;
   o->lista.cantidad = cantidad;
   return o;
 }
@@ -39,84 +43,98 @@ Objeto *nueva_lista(Objeto **elemento, int cantidad) {
 void Liberar_Objeto(Objeto *obj) {
   if (!obj)
     return;
-  if (obj->tipo == TIPO_SIMBOLO)
+
+  switch (obj->tipo) {
+  case TIPO_SIMBOLO:
     free(obj->simbolo);
-  if (obj->tipo == TIPO_LISTA) {
-    for (int i = 0; i < obj->lista.cantidad; i++) {
+    break;
+
+  case TIPO_LISTA:
+    for (int i = 0; i < obj->lista.cantidad; i++)
       Liberar_Objeto(obj->lista.elementos[i]);
-    }
     free(obj->lista.elementos);
+    break;
+
+  default:
+    break;
   }
+
   free(obj);
 }
-/* OPERACIONES BASICAS DEFINIDAS EN EL LENGUAJE */
+
+/* ========= Funciones integradas ========= */
+
 Objeto *builtin_suma(Objeto **args, int argc) {
-  Objeto *res = malloc(sizeof(Objeto));
-  res->tipo = TIPO_NUMERO;
-  res->numero = 0;
+  Objeto *res = nuevo_num(0);
   for (int i = 0; i < argc; i++) {
+    if (args[i]->tipo != TIPO_NUMERO) {
+      printf("Error: '+' solo acepta números\n");
+      return res;
+    }
     res->numero += args[i]->numero;
   }
   return res;
 }
 
 Objeto *builtin_multiplica(Objeto **args, int argc) {
-  Objeto *res = malloc(sizeof(Objeto));
-  res->tipo = TIPO_NUMERO;
-  res->numero = 1;
+  Objeto *res = nuevo_num(1);
   for (int i = 0; i < argc; i++) {
+    if (args[i]->tipo != TIPO_NUMERO) {
+      printf("Error: '*' solo acepta números\n");
+      return res;
+    }
     res->numero *= args[i]->numero;
   }
   return res;
 }
 
-// TODO FIX THIS
-Objeto *evaluar(ASTNODO *nodo) {
-  if (nodo == NULL)
+/* ========= Evaluador ========= */
+
+Objeto *evaluardor(ASTNODO *nodo) {
+  if (!nodo)
     return NULL;
 
   switch (nodo->type) {
-  case NODO_NUMERO: {
-    Objeto *obj = malloc(sizeof(Objeto));
-    obj->tipo = TIPO_NUMERO;
-    obj->numero = nodo->number;
-    return obj;
-  }
+  case NODO_NUMERO:
+    return nuevo_num(nodo->number);
 
-  case NODO_SIMBOLO: {
-    Objeto *obj = malloc(sizeof(Objeto));
-    obj->tipo = TIPO_SIMBOLO;
-    obj->simbolo = strdup(nodo->symbol);
-    return obj;
-  }
+  case NODO_SIMBOLO:
+    return nuevo_simbolo(nodo->symbol);
 
   case NODO_LISTA: {
     if (nodo->lista.contador == 0)
       return NULL;
 
+    // El primer elemento es el "operador" o función
     ASTNODO *primero = nodo->lista.items[0];
-    Objeto *fn = evaluar(primero);
+    Objeto *fn = evaluardor(primero);
 
     int argc = nodo->lista.contador - 1;
     Objeto **args = malloc(sizeof(Objeto *) * argc);
+    for (int i = 0; i < argc; i++)
+      args[i] = evaluardor(nodo->lista.items[i + 1]);
 
-    for (int i = 0; i < argc; i++) {
-      args[i] = evaluar(nodo->lista.items[i + 1]);
-    }
+    Objeto *resultado = NULL;
 
     if (fn->tipo == TIPO_SIMBOLO) {
       if (strcmp(fn->simbolo, "+") == 0) {
-        return builtin_suma(args, argc);
+        resultado = builtin_suma(args, argc);
       } else if (strcmp(fn->simbolo, "*") == 0) {
-        return builtin_multiplica(args, argc);
+        resultado = builtin_multiplica(args, argc);
       } else {
-        printf("Error: función desconocida %s\n", fn->simbolo);
-        exit(1);
+        printf("Error: función desconocida '%s'\n", fn->simbolo);
       }
+    } else {
+      printf("Error: tipo de función no soportado\n");
     }
 
-    printf("Error: tipo de función no soportado\n");
-    exit(1);
+    // Liberar memoria usada temporalmente
+    Liberar_Objeto(fn);
+    for (int i = 0; i < argc; i++)
+      Liberar_Objeto(args[i]);
+    free(args);
+
+    return resultado;
   }
 
   default:
