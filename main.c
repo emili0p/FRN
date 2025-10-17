ed35f05a2b869256d5b6e29ad5d0811f5eebc276
emili0p <emiliioizq23@gmail.com>
2025-10-17 12:11:18 -0600

    modificado el main

--- main.c
+++ main.c
@@ -1,31 +1,32 @@
-#include <stdio.h>
+#include "src/evaluador/evaluador.c"
 #include "src/evaluador/lexer.c"
 #include "src/parser/parser.c"
-#include "src/evaluador/evaluador.c"
+#include <stdio.h>
 
 int main() {
-    const char *codigo = "(defn (x) (* x x))";
+  const char *codigo = "(* (+ 2 3) 4)";
 
-    // Tokenización
-    int num_tokens = 0;
-    char **tokens = tokenizacion(codigo, &num_tokens);
-    int pos = 0;
+  // Tokenización
+  int num_tokens = 0;
+  char **tokens = tokenizacion(codigo, &num_tokens);
+  int pos = 0;
 
-    // Parsing
-    ASTNODO *raiz = parse_tokens(tokens, &pos, num_tokens);
-    imprimir_arbol(raiz, 0);
+  // Parsing
+  ASTNODO *raiz = parse_tokens(tokens, &pos, num_tokens);
+  imprimir_arbol(raiz, 0);
 
-    // Evaluación
-    Objeto *resultado = evaluar(raiz);
-    if (resultado && resultado->tipo == TIPO_NUMERO) {
-        printf("Resultado: %f\n", resultado->numero);
-    }
+  // Evaluación
+  Objeto *resultado = evaluardor(raiz);
+  if (resultado && resultado->tipo == TIPO_NUMERO) {
+    printf("Resultado: %f\n", resultado->numero);
+  }
 
-    // Limpieza
-    Liberar_Objeto(resultado);
-    liberar_ast(raiz);
-    for (int i = 0 ; i < num_tokens; i++) free(tokens[i]);
-    free(tokens);
+  // Limpieza
+  Liberar_Objeto(resultado);
+  liberar_ast(raiz);
+  for (int i = 0; i < num_tokens; i++)
+    free(tokens[i]);
+  free(tokens);
 
-    return 0;
+  return 0;
 }
